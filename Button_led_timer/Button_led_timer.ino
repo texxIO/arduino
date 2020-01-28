f6f97cc909da336fa5701619bd247c01a60b6ece
Ionut M <ionut.mihaescu@gmail.com>
2020-01-28 21:52:24 +0100

    added event1 trigger

--- Button_led_timer/Button_led_timer.ino
+++ Button_led_timer/Button_led_timer.ino
@@ -1,5 +1,7 @@
 const int buttonPin = 2;
 const int ledPin =  4;
+const int event1Pin = 3;
+
 
 int buttonState = 0;
 int buttonStateNew;
@@ -7,15 +9,20 @@ int buttonStateOld = 0;
 int buttonSafeDelay = 100;
 
 int ledState = 0;
+int event1State = 0;
 
 unsigned long startMillis;
 unsigned long currentMillis;
 unsigned long timeDiff;
-const unsigned long ledTimer = 5000; //5 secunde
+const unsigned long ledTimer = 10000; //10 secunde
+const unsigned long eventStartDelay = 5000; // porneste la 5 sec dupa ce a fost apasat butonul
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
   pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(event1Pin, OUTPUT);
+
   startMillis = millis();
 }
 
@@ -28,31 +35,58 @@ void loop() {
 
   if (timeDiff >= ledTimer && ledState == 1)
   {
-    powerOffLed();
+    stopAll();
   }
 
   if (buttonStateOld == 0 && buttonStateNew == 1) {
     startMillis = currentMillis;
 
     if (ledState == 0) {
-      powerOnLed();
+      startLed();
     } else {
-      powerOffLed();
+      stopAll();
     }
-
   }
+
+  triggerEvents();
   buttonStateOld = buttonStateNew;
   delay(buttonSafeDelay);
 }
 
-void powerOffLed()
+void stopLed()
 {
   digitalWrite(ledPin, LOW);
   ledState = 0;
 }
 
-void powerOnLed()
+void startLed()
 {
   digitalWrite(ledPin, HIGH);
   ledState = 1;
+}
+
+void triggerEvents()
+{
+  int startEventTime = startMillis + eventStartDelay;
+  if (ledState == 1 && currentMillis >= startEventTime) {
+    startEvent1();
+  }
+}
+
+void stopAll()
+{
+  stopLed();
+  stopEvent1();
+}
+
+void startEvent1()
+{
+  digitalWrite(event1Pin, HIGH);
+  event1State = 1;
+}
+
+void stopEvent1()
+{
+  digitalWrite(event1Pin, LOW);
+  event1State = 0;
 }
